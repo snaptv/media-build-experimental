e2a7485c446a4893a9af254dd51d270bee95ba9b
Oliver Endriss <o.endriss@gmx.de>
2011-08-27 18:38:22 +0200

    media_build_experimental: Enable CXD2099 by default
    Signed-off-by: Oliver Endriss <o.endriss@gmx.de>

---
+++ experimental/update_kconfig.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use strict;
+
+my $out;
+
+print "media_build_experimental: Modify '.config':\n";
+print "- Enable cxd2099 driver\n";
+
+open IN, '<.config';
+while (<IN>) {
+	s/# CONFIG_STAGING is not set/CONFIG_STAGING=y/;
+	s/# CONFIG_DVB_CXD2099 is not set/CONFIG_DVB_CXD2099=m/;
+	$out .= $_;
+}
+close IN;
+
+open OUT, '>.config';
+print OUT $out;
+close OUT;
